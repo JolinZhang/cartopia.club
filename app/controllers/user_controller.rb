76f31cf7d80f6951e15b9bc281dd176c2f585741
Shane Qi <qizengtai@gmail.com>
2016-04-09 17:25:45 -0500

    Signup validation.

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -3,10 +3,12 @@ class UserController < ApplicationController
   end
 
 	def signup
+		@user = User.new()
 	end
 
 	def create
 		@user = User.new(params.require(:user).permit(:username, :email, :password, :password_confirmation))
+		@user.isadmin = false
 		if @user.save 
 			redirect_to root_path
 		else 
