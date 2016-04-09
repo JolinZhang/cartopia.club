18657bb66168d8ac18e2e4ef4d9ddf7ede80a751
Shane Qi <qizengtai@gmail.com>
2016-04-09 13:49:18 -0500

    Change cars controller name to 'car'.

--- test/controllers/car_controller_test.rb
+++ test/controllers/car_controller_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class CarsControllerTest < ActionController::TestCase
+class CarControllerTest < ActionController::TestCase
   # test "the truth" do
   #   assert true
   # end
