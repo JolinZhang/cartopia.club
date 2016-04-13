5359810e8c1f153daf81f96ecdd085809d17b156
Shane Qi <qizengtai@gmail.com>
2016-04-13 09:06:27 -0500

    Auto login after signup successfully.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,7 @@ class UsersController < ApplicationController
     @user = User.new(params.require(:user).permit(:username, :email, :password, :password_confirmation))
 		@user.isadmin = false
     if @user.save
+      session[:user_id] = @user.id
       redirect_to root_path
     else
       render 'new'
