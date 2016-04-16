ab26a91f88cb2476383b795827f06fdcd7eea084
Shane Qi <qizengtai@gmail.com>
2016-04-16 14:32:06 -0500

    Remember login.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+	attr_accessor :remember_token
 	before_save {
 		self.email = email.downcase
 		self.username = username.downcase
@@ -13,4 +14,25 @@ class User < ActiveRecord::Base
 	validates :password, presence: true, length: { minimum: 6 }, if: :password
   has_many :cars, :dependent => :destroy
 	has_many :favorites, :dependent => :destroy
+
+	def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                  BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
+	def User.new_token
+		SecureRandom.urlsafe_base64
+	end
+	def remember
+		self.remember_token = User.new_token
+		update_attribute(:remember_digest, User.digest(remember_token))
+	end
+	def authenticated?(remember_token)
+		return false if remember_digest.nil?
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+  def forget
+    update_attribute(:remember_digest, nil)
+  end
 end
