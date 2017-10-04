3d5dfefec9c611255fecc27e1f45ed7b4c3c86b1
Shane Qi <qizengtai@gmail.com>
2017-10-04 21:44:09 +0000

    Added Dockerfile.

--- docker/run.sh
+++ docker/run.sh
@@ -1,16 +1,11 @@
-SCRIPT=`readlink -f "$0"`
-SCRIPTPATH=`dirname "$SCRIPT"`
-PROJPATH=`dirname "$SCRIPTPATH"`
 docker run \
 -d \
 --name cartopia \
--v $PROJPATH/:/cartopia.club/ \
+-v `pwd`/:/cartopia.club/ \
 -w /cartopia.club \
-ruby:2.3.0 \
+shaneqi/cartopia:latest \
 /bin/sh -c \
 "\
-apt-get update;\
-apt-get install nodejs -y;\
 bundle install;\
 bin/rake db:migrate RAILS_ENV=development;\
 rm /cartopia.club/tmp/pids/server.pid;\
